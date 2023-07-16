2864e3a9513fb78f0dd1c1e47879352b331e33ba
unknown <olaniyigeorge77@gmail.com>
2023-07-16 23:06:00 +0100

    Search-archive url added to route users to submitted date

--- journal/urls.py
+++ journal/urls.py
@@ -6,7 +6,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('home', views.home, name='home'),
     path("create", views.create_entry, name="create_entry"),
-    path("day/<int:day>/<int:month>/<int:year>", views.day, name='day_view'),
+    path("search-archive", views.search_archive, name="search-archive"),
+    path("day/<int:year>/<int:month>/<int:day>", views.day_view, name='day_view'),
+
     #path("delete/<int:entry_id>", views.delete, name="delete"),
     path("archive/<str:filter_on>", views.archive, name='archive')
 ]
