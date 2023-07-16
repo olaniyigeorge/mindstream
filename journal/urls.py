acba282912c7a481b708910a414a40ceca53510d
unknown <olaniyigeorge77@gmail.com>
2023-07-16 21:37:54 +0100

    Removed the delete url for now

--- journal/urls.py
+++ journal/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('home', views.home, name='home'),
     path("create", views.create_entry, name="create_entry"),
-    path("delete/<int:entry_id>", views.delete, name="delete"),
-    path("<int:day>/<int:month>/<int:year>", views.day, name='day_view'),
+    path("day/<int:day>/<int:month>/<int:year>", views.day, name='day_view'),
+    #path("delete/<int:entry_id>", views.delete, name="delete"),
     path("archive/<str:filter_on>", views.archive, name='archive')
 ]
