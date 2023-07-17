b7fd9863a67947009023cd341ec673141151b6be
unknown <olaniyigeorge77@gmail.com>
2023-07-17 22:23:53 +0100

    ...

--- accounts/managers.py
+++ accounts/managers.py
@@ -9,10 +9,12 @@ class UserManager(BaseUserManager):
         """
         if not email:
             raise ValueError('The Email must be set')
+        
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save()
+        
         return user
         
     def create_superuser(self, email, password, **extra_fields):
