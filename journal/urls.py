60aea05ad68ce56849eecce67f32e4ee350cdae9
unknown <olaniyigeorge77@gmail.com>
2023-07-17 22:25:13 +0100

    Removed unused urls

--- journal/urls.py
+++ journal/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 app_name = "journal"
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('home', views.home, name='home'),
@@ -10,5 +11,5 @@ urlpatterns = [
     path("day/<int:year>/<int:month>/<int:day>", views.day_view, name='day_view'),
 
     #path("delete/<int:entry_id>", views.delete, name="delete"),
-    path("archive/<str:filter_on>", views.archive, name='archive')
+    #path("archive/<str:filter_on>", views.archive, name='archive')
 ]
