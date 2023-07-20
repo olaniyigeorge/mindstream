2bc6342a586fc96971bab74831d3a20fa0fdf97a
unknown <olaniyigeorge77@gmail.com>
2023-07-20 07:40:30 +0100

    Setting up for deployment

--- mindstream/settings.py
+++ mindstream/settings.py
@@ -19,13 +19,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+
+import environ
+
+env = environ.Env()
+environ.Env.read_env() 
+
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-lx1o2eubotnrbpwr1*$l4bm!-@-u&qwy%#jk0=m6_00!7ow_@8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -91,6 +98,17 @@ DATABASES = {
     }
 }
 
+
+# Render's Live PostgreSQL Database
+'''
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.parse(env('DATABASE_URL'))
+}
+
+
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
