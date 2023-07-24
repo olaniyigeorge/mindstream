ca06418d773eec406d30966b15d8945a703cad13
hephhay <faradaydanfard@gmail.com>
2023-07-24 11:21:32 +0100

    fixed create_superuser

--- accounts/managers.py
+++ accounts/managers.py
@@ -22,10 +22,13 @@ class UserManager(BaseUserManager):
         Creates a user, sets is_super to True and saves this superuser 
         with the given email and password.
         """
-        extra_fields.setdefault('is_superuser', True)
-        
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError('Superuser must have is_superuser=True.')
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
         
         return self.create_user(email, password, **extra_fields)
     
