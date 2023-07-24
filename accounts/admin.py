a3c90449e0403e48ab87bc3d19fa2d3a21aa004e
unknown <olaniyigeorge77@gmail.com>
2023-07-24 23:34:04 +0100

    Added OTPCodes to admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import User, UserProfile
+from .models import User, UserProfile, OTPCode
 
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(UserProfile)
+admin.site.register(OTPCode)
