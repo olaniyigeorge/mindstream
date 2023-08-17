0b91255f9504b9d089cd6faf07395cc93b297670
unknown <olaniyigeorge77@gmail.com>
2023-08-17 19:27:35 +0100

    Added functions to handle uri/qrcode generation, OTP verification and validation

--- accounts/mfa_manager.py
+++ accounts/mfa_manager.py
@@ -1,3 +1,7 @@
+import pyotp
+import random
+import qrcode 
+
 #TODO Rewrite to use Class CustomThreeLayerMFA() which takes in a user, saves the layer reached 
 # and when last MFA was tried, is_valid to verify if user has passed the three layers and the 
 #various layers as methods
@@ -18,6 +22,59 @@ class CustomeThreeLayerMFA():
 
 
 
+def make_uri(name, key=pyotp.random_base32()):
+    '''
+    This function takes the user's email and makes an authentication 
+    uri with it.
+    '''
 
+    uri = pyotp.hotp.HOTP(key).provisioning_uri(
+        name = name,
+        issuer_name="Mindstream Journal"
+    )
 
+    print(uri)
     
+
+    return (uri, key)
+    
+
+def make_qrcode_from_uri(uri, filename):
+    qrcodes_save_path = "static/images/qrcodes/"
+    try:
+        qr= qrcode.make(uri)
+        qr.save(f"{qrcodes_save_path}{filename}.png")
+        status = True
+    except:
+        #raise ValueError("Couldn't make or save qrcode")
+        status = False
+    
+    filepath= f"{qrcodes_save_path}{filename}.png"
+    print(f"QRcode created in '{filepath}'")
+    
+    return (status, filepath)
+
+
+def get_key_from_uri(uri):
+    for x in uri:
+        if x == "=":
+            key= uri[(uri.index(x)+1):(uri.index(x)+33)]
+            return key
+        
+
+
+def verify_otp(key, code, counter):
+    hotp = pyotp.HOTP(key)
+    
+    return hotp.verify(code, counter=counter)
+
+
+
+
+def get_otp(key, counter):
+
+    hotp = pyotp.HOTP(key)
+
+    otp = print(f"OTP at {counter}: {hotp.at(int(counter))}")
+    
+    return otp
