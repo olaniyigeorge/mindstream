c7c959eeb72c131547edb2095a900853fc7d69b8
Olaniyi George <olaniyigeorge77@gmail.com>
2023-08-20 02:10:07 +0100

    Svjr to journal in settings.py

--- mindstream/settings.py
+++ mindstream/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =  os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = "mindstream-svjr.onrender.com"
+ALLOWED_HOSTS = "mindstream-journal.onrender.com"
 
 # Application definition
 
