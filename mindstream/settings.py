237c1f087f8526d69e6e682dfc795f714291c5ff
Olaniyi George <olaniyigeorge77@gmail.com>
2023-08-20 03:07:25 +0100

    Staticfile_dir

--- mindstream/settings.py
+++ mindstream/settings.py
@@ -151,9 +151,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # # List of static folders
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static"
-# ]
+ STATICFILES_DIRS = [
+     BASE_DIR / "static"
+ ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
