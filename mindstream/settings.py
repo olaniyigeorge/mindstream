f99d58228a504159737303b38a544929ca287b4c
Olaniyi George <olaniyigeorge77@gmail.com>
2023-08-20 02:02:10 +0100

    Hard coded allowed host in settings.py

--- mindstream/settings.py
+++ mindstream/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =  os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
+ALLOWED_HOSTS = "mindstream-svjr.onrender.com"
 
 # Application definition
 
