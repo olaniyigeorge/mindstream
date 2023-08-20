deb551cf0c87358824762da57b66a69e0666267f
Olaniyi George <olaniyigeorge77@gmail.com>
2023-08-20 03:14:24 +0100

    Update settings.py

--- mindstream/settings.py
+++ mindstream/settings.py
@@ -151,12 +151,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # # List of static folders
- STATICFILES_DIRS = [
-     BASE_DIR / "static"
- ]
+ STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+# https://docs.djangoproject.com/you en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
