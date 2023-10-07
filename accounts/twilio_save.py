8d86ec680be0ae3099ca51073f8b9426c5b79e2c
unknown <olaniyigeorge77@gmail.com>
2023-10-07 10:06:20 +0100

    Added files

--- accounts/twilio_save.py
+++ accounts/twilio_save.py
@@ -1,54 +1,73 @@
 import random
-from twilio.rest import Client
+import pyotp
+import qrcode 
 
-# class Sup():
 
-#     def save(self, *args, **kwargs):
-#             account_sid = 'AC605add792b30fbb291ce5699e84854d7'
-#             auth_token = '45b66a19ccf83ff52aec6a3bceee06a6'
 
 #             client = Client(account_sid, auth_token)
 
-#             validation_request = client.validation_requests \
-#                             .create(
-#                                     friendly_name=f'User{self.owner.pk}',
-#                                     phone_number=f"{self.owner.profile.phone_number}"
-#                                 )
+print(key)
+# totp = pyotp.TOTP(key)
 
-#             print(validation_request.friendly_name)
+# print(totp.now())
 
-#             message = client.messages.create(
-#                 body= f"MINDSTREAM: Verification code for {self.owner.email} your journal {self.code}",
-#                 from_="+12187182316",
-#                 to= f"{self.owner.profile.phone_number}"
-#             )
+name = input("What's your name? ")
 
-#             print(message.sid)
-
-#             return super().save(*args, **kwargs)
-
-
-
-
-
-# account_sid = 'AC605add792b30fbb291ce5699e84854d7'
-# auth_token = '45b66a19ccf83ff52aec6a3bceee06a6'
-
-# client = Client(account_sid, auth_token)
-
-# code = random.randint(100000, 999999)
-# email = "olaniyigeorge77@gmail.com"
-# phone_number = "+2348051410673"
-
-
-# message = client.messages.create(
-#     body= f"MINDSTREAM: Verification code for {email} your journal {code}",
-#     from_="+12187182316",
-#     to= phone_number
+# uri = pyotp.totp.TOTP(key).provisioning_uri(
+#     name = name,
+#     issuer_name="Mindstream Journal"
 # )
 
-# print(message)
-# print(message.sid)
+uri = pyotp.hotp.HOTP(key).provisioning_uri(
+    name = name,
+    issuer_name="Mindstream Journal"
+
+)
+
+print(f"URI: {uri}")
+
+
+qr= qrcode.make(uri).save(f"static/images/qrcodes/{name}.png")
+print("")
+
+hotp = pyotp.HOTP(key)
+
+
+counter= 0
+
+while True:
+    print(f"Initial_count: {hotp.initial_count}")
+    print(f"OTP at Initial_count : {hotp.at(hotp.initial_count)}")
+    
+    print(hotp.verify(input("Enter code: "), counter=counter))
+
+    counter += 1
+
+'''505144
+SIGN UP
+1. On sign up
+2. Set recovery question and answer
+3. Create uri with email
+4. Save uri to profile   // string
+5. Generate QR code with uri and display to user for scanning
+6. Request OTP 
+
+LOGIN 
+1. On login
+2. Get email and password
+3. Get recovery question's answer
+4. Request OTP
+
+FORGOT PASSWORD
+1. 
 
 
 
+pip install django-storages
+pip install boto3
+
+
+
+
+
+'''
